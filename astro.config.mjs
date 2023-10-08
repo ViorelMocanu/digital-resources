f122e2b97b9ea9b7a8026c331ae96331498455d5
Viorel Mocanu <viorel.mocanu@gmail.com>
2023-10-08 03:35:15 +0300

    feat: icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -75,17 +75,17 @@ export default defineConfig({
 				theme_color: ACCENT_COLOR,
 				icons: [
 				{
-					src: 'pwa-192x192.png',
+					src: 'icon-192x192.png',
 					sizes: '192x192',
 					type: 'image/png',
 				},
 				{
-					src: 'pwa-512x512.png',
+					src: 'icon-512x512.png',
 					sizes: '512x512',
 					type: 'image/png',
 				},
 				{
-					src: 'pwa-512x512.png',
+					src: 'icon-512x512.png',
 					sizes: '512x512',
 					type: 'image/png',
 					purpose: 'any maskable',
