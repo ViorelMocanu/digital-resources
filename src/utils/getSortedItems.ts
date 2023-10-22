85bb3fbcea0f40ac5d0922c916e99caed120e5e6
Petre Gabriel <petregabriel1407@gmail.com>
2023-10-22 23:55:50 +0300

    refactor: added sortValueFromA/B instead of dataFromA/B[sortKey]

--- src/utils/getSortedItems.ts
+++ src/utils/getSortedItems.ts
@@ -18,17 +18,17 @@ type Props = {
 
 export default async function getSortedItems({ collectionKey, sortKey, type = 'asc', isDate = false }: Props) {
 	return (await getCollection(collectionKey)).sort((a, b) => {
-		const { data: dataFromA } = a
-		const { data: dataFromB } = b
+		const { data: { [sortKey]: sortValueFromA } } = a
+		const { data: { [sortKey]: sortValueFromB } } = b
 
 		if (isDate) {
 			if (type === 'asc')
-				return (new Date(dataFromA[sortKey] as string).valueOf() - new Date(dataFromB[sortKey] as string).valueOf())
-			return (new Date(dataFromB[sortKey] as string).valueOf() - new Date(dataFromA[sortKey] as string).valueOf())
+				return (new Date(sortValueFromA).valueOf() - new Date(sortValueFromB).valueOf())
+			return (new Date(sortValueFromB).valueOf() - new Date(sortValueFromA).valueOf())
 		}
 
 		if (type === 'asc')
-			return dataFromA[sortKey] - dataFromB[sortKey]
-		return dataFromB[sortKey] - dataFromA[sortKey]
+			return sortValueFromA - sortValueFromB
+		return sortValueFromB - sortValueFromA
 	});
 };
