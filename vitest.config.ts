cc2ee17f1a4632f180db266eadd402370a1f9c8b
Viorel Mocanu <viorel.mocanu@gmail.com>
2024-05-22 11:22:38 +0300

    chore: update files after testing

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference types="vitest" />
 import { configDefaults } from 'vitest/config';
 import { getViteConfig } from 'astro/config';
